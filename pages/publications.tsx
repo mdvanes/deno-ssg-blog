f3df7d71b329e289e1e0c91323bf91c320739b14
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-02-09 15:00:28 +0100

    Add fetch

--- pages/publications.tsx
+++ pages/publications.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppTopBar from "../components/AppTopBar.tsx";
 import {
   Container,
@@ -8,7 +8,18 @@ import {
   Typography,
 } from "https://esm.sh/@material-ui/core@4.11.3";
 
+const getPublications = async () => {
+  const feed = await fetch("/test.json");
+  console.log(await feed.json());
+//   const feed = await fetch("https://medium.com/feed/codestar-blog");
+};
+
 export default function Publications() {
+  useEffect(() => {
+    console.log("on load");
+    getPublications();
+  }, []);
+
   return (
     <>
       <AppTopBar />
