744abba9ae93997aae4f8b6b4df90cc66d7c1dfc
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-03-02 12:42:58 +0100

    Disable unsupported imports

--- app.tsx
+++ app.tsx
@@ -7,7 +7,7 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
     <main>
       <head>
         <title>Codestar [Aleph]</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" /> */}
         <link rel="stylesheet" href="./style/index.css" />
       </head>
       {/* Styling does not work when <AppTopBar /> is loaded in this module? */}
