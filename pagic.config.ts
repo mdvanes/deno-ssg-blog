8b65e10bf8294eb1eb38803c29f9460d77b7db56
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-03-06 13:28:59 +0100

    Update pagic.config.ts

--- pagic.config.ts
+++ pagic.config.ts
@@ -1,6 +1,7 @@
 export default {
   include: ['README.md', 'docs', 'posts'],
   root: "/deno-ssg-blog/",
+  title: "deno ssg Blog",  description: 'Welcome to my blog, where I have collected my technical articles and life insights.',
   theme: "blog",
   plugins: ['blog'],
    nav: [    {      text: 'Homepage',      link: '/index.html',      icon: 'czs-home-l',    },    {      text: 'Categories',      link: '/categories/index.html',      icon: 'czs-category-l',    },    {      text: 'Tags',      link: '/tags/index.html',      icon: 'czs-tag-l',    },    
