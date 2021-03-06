3cbf8be1977275df67558e0238d6606fd1c3982d
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-03-06 15:13:20 +0100

    Update pagic.config.ts

--- pagic.config.ts
+++ pagic.config.ts
@@ -7,4 +7,11 @@ export default {
    nav: [    {      text: 'Homepage',      link: '/index.html',      icon: 'czs-home-l',    },    {      text: 'Categories',      link: '/categories/index.html',      icon: 'czs-category-l',    },    {      text: 'Tags',      link: '/tags/index.html',      icon: 'czs-tag-l',    },    
          {      text: 'About',      link: '/about/index.html',      icon: 'czs-about-l',    },    
    ],
+  blog: {
+   social: {
+      github: 'xcatliu/blog',
+      email: 'xcatliu@gmail.com',
+      twitter: 'xcatliu',
+    }
+  },
 }; 
