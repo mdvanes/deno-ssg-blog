22e3bb88942e49002cfa73d123a82cbd41438b68
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-03-23 13:03:45 +0100

    Update config

--- pagic.config.ts
+++ pagic.config.ts
@@ -1,17 +1,25 @@
 export default {
-  exclude: ['api', 'components', 'lib', 'pages', 'public','style', 'app.tsx'],
+  exclude: ["api", "components", "lib", "pages", "public", "style", "app.tsx"],
   root: "/deno-ssg-blog/",
-  title: "Pagic ssg Blog",  description: 'Welcome to my blog, where I have collected my technical articles and life insights.',
+  title: "Pagic ssg Blog",
+  description:
+    "Web development blog since 2003",
   theme: "blog",
-  plugins: ['blog'],
-   nav: [    {      text: 'Homepage',      link: '/index.html',      icon: 'czs-home-l',    },    {      text: 'Categories',      link: '/categories/index.html',      icon: 'czs-category-l',    },    {      text: 'Tags',      link: '/tags/index.html',      icon: 'czs-tag-l',    },    
-         {      text: 'About',      link: '/about/index.html',      icon: 'czs-about-l',    },    
-   ],
+  plugins: ["blog"],
+  nav: [
+    { text: "Homepage", link: "index.html", icon: "czs-home-l" },
+    {
+      text: "Categories",
+      link: "categories/index.html",
+      icon: "czs-category-l",
+    },
+    { text: "Tags", link: "/tags/index.html", icon: "czs-tag-l" },
+    { text: "About", link: "/about/index.html", icon: "czs-about-l" },
+  ],
   blog: {
-   social: {
-      github: 'xcatliu/blog',
-      email: 'xcatliu@gmail.com',
-      twitter: 'xcatliu',
-    }
+    social: {
+      github: "mdvanes",
+      twitter: "mdworldNL",
+    },
   },
-}; 
+};
